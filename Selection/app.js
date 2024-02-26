6e29bcb9bc2b5f054661237193ef0979eeea9df1
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-26 18:33:55 +0700

    27/02/24: Mendapatkan Element Html Berdasarkan Tag Dan Class

--- Selection/app.js
+++ Selection/app.js
@@ -1,22 +1,18 @@
+// Belajar Java Script DOM
+// GetElementByTagName
+
 // const allImages = document.getElementsByTagName('img');
 
 // for (let img of allImages) {
-// 	// console.log(img.src);
-// 	img.src =
-// 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
+//    // console.log(img.src)
+//    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg'
 // }
 
-// const squareImages = document.getElementsByClassName('square');
+// getElementByClassName
 
-// for (let img of squareImages) {
-// 	img.src =
-// 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
-// }
+const squareImages = document.getElementsByClassName('square');
 
-const links = document.querySelectorAll('a');
-
-for (let link of links) {
-	link.style.color = 'rgb(0, 108, 134)';
-	link.style.textDecorationColor = 'magenta';
-	link.style.textDecorationStyle = 'wavy';
+for (let img of squareImages) {
+   img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg'
 }
+
