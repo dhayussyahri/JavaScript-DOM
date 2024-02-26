68dc7421bb3f2e7dda2f6d48a4f01ba9a1cb868d
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-26 19:07:07 +0700

    26/02/24: Memahami Innerthtml, Innertext Dan Textcontent

--- Selection/app.js
+++ Selection/app.js
@@ -10,9 +10,26 @@
 
 // getElementByClassName
 
-const squareImages = document.getElementsByClassName('square');
+// const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-   img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg'
+// for (let img of squareImages) {
+//    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg'
+// }
+
+// *******************************************************************
+// Lebih Mudah Memilih Element Dengan Menggunakan Queryselector
+
+// const links = document.querySelectorAll('p a');
+
+// for (let link of links) {
+//    console.log(link.href)
+// }
+
+// *****************************************************************
+// Memahami Innerthtml, Innertext Dan Textcontent
+
+const links = document.querySelectorAll('p a');
+
+for (let link of links) {
+   link.innerText = 'Saya adalah link!';
 }
-
