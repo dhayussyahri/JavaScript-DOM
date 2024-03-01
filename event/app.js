bfc1c04a97046f5e159db8e05c73cf751ba08f40
dhayus <74321000+dhayus@users.noreply.github.com>
2024-03-01 20:02:50 +0700

    01/03/24: Belajar JavaScript DOM - AddEventListener

--- event/app.js
+++ event/app.js
@@ -4,5 +4,33 @@ const button = document.querySelector('#clickme');
 
 
 button.onclick = function () {
-   console.log('You can click me!');
+   console.log('You can click me')
 }
+
+// Cara kedua
+
+const scream = () => {
+   console.log('Dont touch me')
+}
+
+button.onmouseover = scream;
+
+// **********************************************
+
+// Belajar Javascript DOM - AdEventListener
+
+const eventbtn = document.querySelector('#eventbtn');
+
+eventbtn.addEventListener('click', stepSatu);
+eventbtn.addEventListener('dblclick', stepDua);
+
+function stepSatu() {
+   console.log('Step One Succes');
+}
+
+
+function stepDua() {
+   console.log('Step Two Succes');
+}
+
+// **********************************************
