4be73dcce4b449fc03328bee5bbeffe74a6a506b
dhayus <74321000+dhayus@users.noreply.github.com>
2024-03-04 17:05:53 +0700

    04/03/24: Belajar JavaScript DOM - Event

--- keyword/app.js
+++ keyword/app.js
@@ -1,4 +1,6 @@
 // Belajar Javascript DOM - Manfaatkan Keyword This Pada Event Dom
+const buttons = document.querySelectorAll('button');
+const headings = document.querySelectorAll('h1');
 
 const generateRandomColor = () => {
    const r = Math.floor(Math.random() * 255);
@@ -6,5 +8,33 @@ const generateRandomColor = () => {
    const b = Math.floor(Math.random() * 255);
 
    return `rgb(${r}, ${g}, ${b})`;
-
 }
+
+function colorize() {
+   this.style.backgroundColor = generateRandomColor();
+   this.style.color = generateRandomColor();
+}
+
+for(let button of buttons) {
+   button.addEventListener('click', colorize);
+}
+
+for (let heading of headings) {
+   heading.addEventListener('click', colorize);
+}
+//* perulangan
+// for (let button of buttons) {
+//    button.addEventListener('click', () => {
+//       button.style.backgroundColor = generateRandomColor();
+//       button.style.color = generateRandomColor();
+//       console.log(this);
+//    })
+// }
+
+
+// for (let heading of headings) {
+//    heading.addEventListener('click', () => {
+//       heading.style.backgroundColor = generateRandomColor();
+//       heading.style.color = generateRandomColor();
+//    });
+// }
