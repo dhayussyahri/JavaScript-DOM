e8fb5756b23c7951d403b8fdb54d9ce9aefae8e0
dhayus <74321000+dhayus@users.noreply.github.com>
2024-03-05 14:33:31 +0700

    04/03/24: Praktek Input Realtime Dengan Event Dan Dom

--- event prevent default/apps.js
+++ event prevent default/apps.js
@@ -1,11 +1,27 @@
 // Mengenal Event Preventdefault
 const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const list = document.querySelector('#notes');
+
+
+input.addEventListener('change', (e) => {
+   console.log('Nilai Berubah');
+});
+
+
 form.addEventListener('submit', function (e) {
    e.preventDefault();
-   console.log('submitted form');
+   const noteValue = input.value;
+   const newlist = document.createElement('li');
+   newlist.innerText = noteValue;
+
+   list.append(newlist);
+   input.value = '';
 });
-document.querySelector('a').addEventListener('click', (e) => {
-   e.preventDefault();
-});
+
+
+// document.querySelector('a').addEventListener('click', (e) => {
+//    e.preventDefault();
+// });
 
 // event Prevent default = untuk mencegah proses selanjutnya dari sebuah interaksi behavior default
