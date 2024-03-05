a3fee8516f9b1394a59b68f385bce2bc08a65db7
dhayus <74321000+dhayus@users.noreply.github.com>
2024-03-05 19:15:59 +0700

    05/03/2024: Mengenal Event Delegation

--- event prevent default/apps.js
+++ event prevent default/apps.js
@@ -8,22 +8,24 @@ const list = document.querySelector('#notes');
 //    console.log('Nilai Berubah');
 // });
 
-input.addEventListener('input', (e) => {
-   console.log('Nilai diinput');
-});
+// input.addEventListener('input', (e) => {
+//    console.log('Nilai diinput');
+// });
 
 
 form.addEventListener('submit', function (e) {
    e.preventDefault();
    const noteValue = input.value;
-   const newlist = document.createElement('li');
-   newlist.innerText = noteValue;
+   const newList = document.createElement('li');
+   newList.innerText = noteValue;
 
-   list.append(newlist);
+   list.append(newList);
    input.value = '';
 });
 
-
+list.addEventListener('click', (e) => {
+   e.target.nodeName === 'LI' && e.target.remove(); // untuk menghapus bagian Attributnya saja
+});
 // document.querySelector('a').addEventListener('click', (e) => {
 //    e.preventDefault();
 // });
